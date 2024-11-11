dc48950a0775c1e182ee66fae8d4b7f948d23639
pacoweb1 <callejas.paco.victor.rolando@itsecsucre.edu.bo>
2024-11-10 22:17:22 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.');
+});
